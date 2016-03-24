3f548bf70f7a168bcca7153a16273ea63147baff
Alosto <cardilloab@gmail.com>
2016-03-24 00:16:48 -0500

    Update pyelo.py
    Updated code, improved comments

--- pyelo.py
+++ pyelo.py
@@ -1,42 +1,18 @@
 '''
 PyElo Library for easy implementation of Elo systems.
+View the README for documentation, and check out example.py to see a common chess example.
+
 @author: Tony Cardillo, cardilloab@gmail.com
 
-Functions:
+Function List:
 
-def setMean(mean)
-    returns: None
-    This sets the starting Elo of all future players, and represents what the average player will be ranked.
-    If you reset a player, it will also reset to this value. By default, this value is 1000.
-    
+def setMean(mean)  
 def setK(K)
-    returns: None
-    This sets the K-factor used to determine how dynamic the Elo system is at responding to change. High values lead to larger
-    fluctuations that may over-respond, and small values lead to small fluctuations that may lack sensitivity. 
-    In chess, K ranges anywhere from 10 to 40, depending on the rating organization and professional status of the players.
-    By default, this value is 20.
-  
 def setRPA(RPA)
-    returns: None
-    The RPA, set to 400 in most systems, represents the difference in Elo ratings between 2 players that will result in the
-    higher rated player winning 10 times as often. Thus, with an RPA of 400, a match between an 1800 and 1400 player will
-    result in the 1800 player winning 10 times more. By default, this value is 400.
-    
+def getPlayerOdds(teamA,teamB)
 def createPlayer(handle)
-    returns: a class
-    handle is the reference to a player or team you want to start ranking. It may be a string of the player's name, for example.
-     
-def rankPlayers():     
-    returns: a descending sorted list of all the players currently ranked in your Elo system 
-
-def addGameResults(teamA,teamAScore,teamB,teamBScore,homeAdvA,homeAdvB,mov):
-    teamA is the class returned from createPlayer()
-    teamAScore is the score of team A. In chess, a win is 1, a loss is 0, and a draw is 0.5.
-    teamB and teamBScore are the same format as teamA.
-    homeAdvA is a flag for whether teamA has a home advantage. 1 means a home game, -1 means an away game, and 0 is a neutral location.
-    homeAdvB is a flag for teamB's home advantage and follows the same format as above.
-    mov stands for Margin of Victory, and is a flag for whether to take into account the size of the victory. 1 means yes, 0 means no.
-        You would use 0 for chess, and probably 1 for games such as basketball, football/soccer, etc.
+def rankPlayers()   
+def addGameResults(teamA,teamAScore,teamB,teamBScore,homeAdvA,homeAdvB,mov)
 '''
 
 # Import math functionality, used for exponentiation
@@ -120,6 +96,9 @@ class Elo:
             teamA.elo = teamA.elo + eloSystem.K*(win - expectedScore)
             teamB.elo = teamB.elo + eloSystem.K*((1-win) - expectedScore)
 
+# Setup our default Elo system with some commonly used values
+eloSystem = Elo(1000,20,400)
+
 # The Player class contains generic data about the player's Elo rating, wins/losses, and even upsets
 # Not all of these variables must be used, but they are kept track of automatically anyway
 class Player:
@@ -151,32 +130,74 @@ class Player:
 playerList = []
 
 def setMean(mean):
-    # You can change the starting Elo for all future players, or leave it at the default.
+    ''' returns: None
+    This sets the starting Elo of all future players, and represents what the average player will be ranked.
+    If you reset a player, it will also reset to this value. By default, this value is 1000.
+    ''' 
     eloSystem.mean = mean
     
+    
 def setK(K):
-    # You can change the K-factor used for all future players, or leave it at the default.
+    ''' returns: None
+    This sets the K-factor used to determine how dynamic the Elo system is at responding to change. High values lead to larger
+    fluctuations that may over-respond, and small values lead to small fluctuations that may lack sensitivity. 
+    In chess, K ranges anywhere from 10 to 40, depending on the rating organization and professional status of the players.
+    By default, this value is 20.
+    '''
     eloSystem.K = K
     
+    
 def setRPA(RPA):
-    # You can change the "rating point amount" for all future players, or leave it at the default.
+    ''' returns: None
+    The RPA, set to 400 in most systems, represents the difference in Elo ratings between 2 players that will result in the
+    higher-rated player winning 10 times as often. Thus, with an RPA of 400, a match between an 1800 and 1400 player will
+    result in the 1800 player winning 10 times more often. By default, this value is 400.
+    '''
     eloSystem.RPA = RPA
     
+    
 def createPlayer(handle):
-    newPlayer = Player(handle) # Handle can be a string, a class, whatever (this makes PyElo more flexible)
+    ''' returns: a class Player() 
+    handle is the reference to a player or team you want to start ranking. It may be a string of the player's name, for example.
+    The new player is automatically added to playerList, a list of all players that may be sorted.
+    '''
+    newPlayer = Player(handle)
     playerList.append(newPlayer)
     return newPlayer
      
+     
 def rankPlayers():     
-    # Returns a descending order list of all the players ranked by their Elo rating
+    ''' returns: a descending sorted list of all the players currently ranked in your Elo system. 
+    '''
     return sorted(playerList, key=lambda player: player.elo, reverse=True) 
 
+
 def getPlayerOdds(teamA,teamB):
-    # The odds of teamA/playerA beating teamB/playerB
+    ''' returns: a float from 0.0 to 100.0 that represents the percentage odds that teamA will beat teamB 
+    '''
     return 100.0 / (1.0 + math.pow(10.0,(teamB.elo-teamA.elo)/eloSystem.RPA))   
 
-def addGameResults(teamA,teamAScore,teamB,teamBScore,homeAdvA,homeAdvB,mov):
+
+def addGameResults(teamA,teamAScore,teamB,teamBScore,homeAdvA=0,homeAdvB=0,mov=0):
+    ''' returns: None
+        teamA is the class returned from createPlayer()
+        teamAScore is the score of team A. In chess, a win is 1, a loss is 0, and a draw is 0.5.
+        teamB and teamBScore are the same format as teamA.
+        homeAdvA is an optional flag for whether teamA has a home advantage. 1 means a home game, -1 means an away game, and 0 is a neutral location (default).
+        homeAdvB is an optional flag for teamB's home advantage and follows the same format as above.
+        mov stands for Margin of Victory, and is an optional flag for whether to take into account the size of the victory. 1 means yes, 0 means no (default).
+            You would use 0 for chess, and probably 1 for games such as basketball, football/soccer, etc.
+    '''
     eloSystem.addGame(teamA,teamAScore,teamB,teamBScore,homeAdvA,homeAdvB,mov)
     
+    
 def setHomeAdvAmt(homeAdvAmt):
+    ''' returns: None
+        This sets the amount, in Elo points, that a home-advantage team is expected to have.
+        In chess, there is no well known home team advantage. 
+        For other sports, set homeAdvAmt equal to your K-factor (default: 20) times the number of game points advantage.
+            In basketball, this might be 60 Elo (20 * 3 baskets = 60 Elo)
+            This is an inexact science, so results may vary.
+    '''
     eloSystem.homeAdvAmt = homeAdvAmt
+    
