43b6aabf414b4e65f3fd1a9120f8f3c43eee2aaa
Alosto <cardilloab@gmail.com>
2016-03-24 00:14:30 -0500

    Update example.py
    Updated example code with more/better comments; interprets correctly

--- example.py
+++ example.py
@@ -1,25 +1,30 @@
-# Import our PyElo library
+# Import the PyElo library
 import pyelo
 
-# First, our Elo system is already setup with some default, commonly used values, but these
-# optional functions can change them if desired:
+# Our Elo system is, by default, already setup with some default, commonly used values,
+# but these optional functions can change them if desired:
 pyelo.setMean(1000)
 pyelo.setK(20)
 pyelo.setRPA(400)
 # Again, these above functions are optional. See wikipedia for how these variables might be changed in your program.
    
-    
+   
 # Create two players for demonstration
-adam = createPlayer("Adam")
-bob = createPlayer("Bob")
+adam = pyelo.createPlayer("Adam")
+bob = pyelo.createPlayer("Bob")
 
 # Lets add a chess game where Adam won, Bob lost, and no player had a home advantage
-# Arguments: playerA,playerAScore,playerB,playerBScore,homeAdvA,homeAdvB,mov (margin of victory)
-pyelo.addGameResults(adam,1,bob,0,0,0,0)
+pyelo.addGameResults(adam, # playerA
+                     1,    # playerAScore (1 = win in chess)
+                     bob,  # playerB
+                     0,    # playerBScore (0 = loss in chess)
+                     0,    # homeAdvA (optional)
+                     0,    # homeAdvB (optional)
+                     0)    # mov (margin of victory, optional)
 
 # Here are the results!
 print("After Game 1, where Adam won, the Elo scores are now:")
 print(pyelo.rankPlayers())
-print("The odds of Adam beating Bob in another game are now:")
+print("Thus, the odds of Adam beating Bob in another game are now:")
 print(str(pyelo.getPlayerOdds(adam,bob))+"%")
     
